aba90e106f4b4bb7603afdf361ff3c2c9913febc
bobrofon <bobrofon@gmail.com>
2020-01-15 17:49:15 +0300

    meson: Add cross-compile file

--- conanfile.py
+++ conanfile.py
@@ -1,54 +1,76 @@
+import os
+
 from conans import ConanFile, tools, Meson
+
+import tools.meson as meson_tools
 
 
 class LibfuseConan(ConanFile):
-    name = "fuse3"
-    version = "3.7.0"
-    license = "LGPLv2"
-    author = "bobrofon@gmail.com"
-    url = "https://github.com/bobrofon/fuse3"
-    description = "Linux FUSE (Filesystem in Userspace) interface"
-    topics = ("fuse", "fs")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False],
-               "disable_mtab": [True, False],
-               "examples": [True, False],
-               "udevrulesdir": "ANY",
-               "useroot": [True, False],
-               "utils": [True, False]}
-    default_options = {"shared": False,
-                       "disable_mtab": False,
-                       "examples": False,
-                       "udevrulesdir": "/etc/udev/rules.d",
-                       "useroot": True,
-                       "utils": True}
-    generators = "pkg_config"
-    build_requires = "meson_installer/0.51.0@bincrafters/stable"
+    name = 'fuse3'
+    version = '3.7.0'
+    license = 'LGPLv2'
+    author = 'bobrofon@gmail.com'
+    url = 'https://github.com/bobrofon/fuse3-conan'
+    description = 'Linux FUSE (Filesystem in Userspace) interface'
+    topics = ('fuse', 'fs')
+    settings = ('os', 'compiler', 'build_type', 'arch')
+    options = {'shared': [True, False],
+               'disable_mtab': [True, False],
+               'examples': [True, False],
+               'udevrulesdir': 'ANY',
+               'useroot': [True, False],
+               'utils': [True, False]}
+    default_options = {'shared': False,
+                       'disable_mtab': False,
+                       'examples': False,
+                       'udevrulesdir': '/etc/udev/rules.d',
+                       'useroot': False,
+                       'utils': True}
+    generators = 'pkg_config'
+    build_requires = 'meson_installer/0.51.0@bincrafters/stable'
+
+    exports = 'tools/*.py'
+
+    cross_file_name = 'cross_file.txt'
+
+    meson = None
 
     def source(self):
-        git = tools.Git(folder="fuse")
-        git.clone("https://github.com/libfuse/libfuse.git", "fuse-3.7.0")
+        git_tag = 'fuse-' + self.version
+
+        git = tools.Git(folder='fuse')
+        git.clone('https://github.com/libfuse/libfuse.git', git_tag)
+
+        # TODO: remove
+        tools.replace_in_file('fuse/util/meson.build',
+"""meson.add_install_script('install_helper.sh',
+                         join_paths(get_option('prefix'), get_option('sysconfdir')),
+                         join_paths(get_option('prefix'), get_option('bindir')),
+                         udevrulesdir,
+                         '@0@'.format(get_option('useroot')))""", '')
 
     def build(self):
-        meson = Meson(self)
-        meson.configure(source_folder="fuse", build_folder="build",
-                        args=["-D", "disable-mtab={}".format("true" if self.options.disable_mtab else "false"),
-                              "-D", "examples={}".format("true" if self.options.examples else "false"),
-                              "-D", "udevrulesdir={}".format(self.options.udevrulesdir),
-                              "-D", "useroot={}".format("true" if self.options.useroot else "false"),
-                              "-D", "utils={}".format("true" if self.options.utils else "false")])
-        meson.build()
+        args = ['-D', 'disable-mtab=' + str(self.options.disable_mtab).lower(),
+                '-D', 'examples=' + str(self.options.examples).lower(),
+                '-D', 'udevrulesdir=' + str(self.options.udevrulesdir),
+                '-D', 'useroot=' + str(self.options.useroot).lower(),
+                '-D', 'utils=' + str(self.options.utils).lower()]
+
+        if tools.cross_building(self.settings):
+            meson_tools.write_cross_file(self.cross_file_name, self)
+            args += ['--cross-file', 'cross_file.txt']
+
+        self.meson = Meson(self)
+        self.meson.configure(source_folder='fuse',
+                             build_folder='build',
+                             args=args,
+                             defs=meson_tools.common_flags(self.settings))
+        self.meson.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="fuse/include")
-        self.copy("*fuse3.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*fuse3.pc", dst="", keep_path=False)
-        self.copy("*fusermount3", dst="bin", keep_path=False)
+        self.meson.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["fuse3", "dl", "pthread"]
-        self.cpp_info.defines = ["_FILE_OFFSET_BITS=64"]
+        self.cpp_info.libs = ['fuse3', 'dl', 'pthread']
+        self.cpp_info.defines = ['_FILE_OFFSET_BITS=64']
+        self.cpp_info.includedirs.append(os.path.join('include', 'fuse3'))
